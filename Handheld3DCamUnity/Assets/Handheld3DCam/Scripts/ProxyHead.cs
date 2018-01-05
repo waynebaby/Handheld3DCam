b4928f4ee691a0b3bba8028543e9439dbeaa9a96
Wayne Wang <waywa@microsoft.com>
2018-01-05 23:13:23 +0800

    V2, can rorate proxy eyes when you turn your cam  with z axis

--- Handheld3DCamUnity/Assets/Handheld3DCam/Scripts/ProxyHead.cs
+++ Handheld3DCamUnity/Assets/Handheld3DCam/Scripts/ProxyHead.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using HoloToolkit.Unity;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,14 +9,22 @@ public class ProxyHead : MonoBehaviour
     // Use this for initialization
     public GameObject leftEye;
     public GameObject rightEye;
-
+    public Vector3 Projected;
 
     // Update is called once per frame
     void Update()
     {
         var direction = HandheldScreen.Instance.transform.position - Camera.main.transform.position;
+        var mcTrans = CameraCache.Main.transform;
+        var camleft = mcTrans.position - mcTrans.rotation * (Vector3.left * 0.031f);
+        var camright = mcTrans.position + mcTrans.rotation * (Vector3.left * 0.031f);
+        var eyesVector = camleft - camright;
+        Projected = Vector3.ProjectOnPlane(eyesVector, direction);
 
-        
+        //var rot = Quaternion.Euler(projected.normalized);
+
+        //gameObject.transform.localRotation = rot;
+        rightEye.transform.localPosition = leftEye.transform.localPosition  + Projected;
 
     }
 }
